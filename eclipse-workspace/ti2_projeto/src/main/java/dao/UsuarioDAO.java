9c6a4e531a0038c2ca4c598a0dd5d0a985bde2f8
LucasAlkmimBarros <lucasalk2005@gmail.com>
2023-10-25 21:57:05 -0300

    Updates (usuario pronto)

--- eclipse-workspace/ti2_projeto/src/main/java/dao/UsuarioDAO.java
+++ eclipse-workspace/ti2_projeto/src/main/java/dao/UsuarioDAO.java
@@ -35,6 +35,57 @@ public class UsuarioDAO extends DAO{
 		return status;
 	}
 
+	public boolean delete(int id){
+		boolean status = false;
+		try {
+			Statement st = conexao.createStatement();
+			st.executeUpdate("DELETE FROM usuario WHERE id = " + id);
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
 
-	
+	public boolean update(Usuario usuario){
+		boolean status = false;
+		try {
+			String sql = "UPDATE usuario SET username = '" + usuario.getUsername() + "', nome = '"  
+			+ usuario.getNome() + "', email = '" + usuario.getEmail() + "', idade = " + usuario.getIdade() + ", genero = '" + usuario.getGenero() + "', foto = '" + usuario.getFoto() + "', senha = '" + usuario.getSenha() + "'"
+			+ " WHERE id = " + usuario.getId();
+			PreparedStatement st = conexao.prepareStatement(sql);
+			st.executeUpdate();
+			st.close();
+			status = true;
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return status;
+	}
+
+	public Usuario get(int id){
+		System.out.println("ID: " + id);
+		Usuario usuario = null;
+		try {
+			Statement st = conexao.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM usuario WHERE id = " + id);
+			if(rs.next()){
+				usuario = new Usuario(
+					rs.getInt("id"),
+					rs.getString("username"),
+					rs.getString("nome"),
+					rs.getString("email"),
+					rs.getInt("idade"),
+					rs.getString("genero"),
+					rs.getString("foto"),
+					rs.getString("senha")
+				);
+			}
+			st.close();
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+		return usuario;
+	}
 }
--- eclipse-workspace/ti2_projeto/src/main/java/service/UsuarioService.java
+++ eclipse-workspace/ti2_projeto/src/main/java/service/UsuarioService.java
@@ -13,26 +13,66 @@ import com.google.gson.Gson;
 
 public class UsuarioService {
 
-        public UsuarioDAO usuarioDAO = new UsuarioDAO();
+	public UsuarioDAO usuarioDAO = new UsuarioDAO();
 
-        public UsuarioService(){
+	public UsuarioService(){
 
-        }
+	}
 
-        public String insert(Request request, Response response){
+	public String insert(Request request, Response response){
+		String str = request.body();
 
-			System.out.println("Conexão efetuada com o service!");
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(str, Usuario.class);
+		
+		if(usuarioDAO.insert(usuario) == true){
+			return "Usuário inserido com sucesso!";
+		}
+		else{
+			return "Erro ao inserir usuário!";
+		}
+	}
 
-                String str = request.body();
+	public String delete(Request request, Response response){
+		String str = request.body();
 
-                Gson gson = new Gson();
-                Usuario usuario = gson.fromJson(str, Usuario.class);
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(str, Usuario.class);
+		
+		if(usuarioDAO.delete(usuario.getId()) == true){
+			return "Usuário deletado com sucesso!";
+		}
+		else{
+			return "Erro ao deletar usuário!";
+		}
+	}
 
-                if(usuarioDAO.insert(usuario) == true){
-                        return "Usuário inserido com sucesso!";
-                }
-                else{
-                        return "Erro ao inserir usuário!";
-                }
-        }
-}
+	public String update(Request request, Response response){
+		String str = request.body();
+
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(str, Usuario.class);
+		
+		if(usuarioDAO.update(usuario) == true){
+			return "Usuário atualizado com sucesso!";
+		}
+		else{
+			return "Erro ao atualizar usuário!";
+		}
+	}
+
+	public String get(Request request, Response response){
+		String str = request.body();
+
+		Gson gson = new Gson();
+		Usuario usuario = gson.fromJson(str, Usuario.class);
+		
+		Usuario usuarioBuscado = usuarioDAO.get(usuario.getId());
+		if(usuarioBuscado != null){
+			return "Usuário encontrado: " + usuarioBuscado.getNome();
+		}
+		else{
+			return "Usuário não encontrado!";
+		}
+	}
+} 
